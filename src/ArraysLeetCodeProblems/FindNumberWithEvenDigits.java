b46a7abd173b11d2ae8c134514af5f80d8839df5
Your New Name <Venkatesh.Swaminathan@McKesson.com>
2025-07-23 13:17:40 +0530

    leetCode/FindNumberOfEvenDigits

--- src/ArraysLeetCodeProblems/FindNumberWithEvenDigits.java
+++ src/ArraysLeetCodeProblems/FindNumberWithEvenDigits.java
@@ -1,19 +1,39 @@
 package ArraysLeetCodeProblems;
 
+
 /*
  * https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/
  */
 public class FindNumberWithEvenDigits {
 	public static void main(String[] args) {
-		System.out.println("The count is : " + countTheDigits(345909876));
+		int[ ] val= {12,678,5679,1,297,456098};
+		int count=findNumbers(val);
+        System.out.println(count);
 	}
-
-	static int countTheDigits(int num) {
-		int count = 0;
-		while (num > 0) {
-			count++;
-			num = num / 10;
-		}
-		return count;
-	}
+    static int findNumbers(int[] nums) {
+        int count=0;
+        for(int num:nums){
+            if(validateEvenDigits(num)){
+                count++;
+            }
+        }
+        return count;
+    }
+    static boolean validateEvenDigits(int num){
+        return countTheDigits(num) % 2 == 0;
+    }
+    static int countTheDigits(int num){
+        int count=0;
+        if(num<0){
+            return -1;
+        }
+        if(num==0){
+            return 1;
+        }
+        while(num>0){
+            count++;
+            num=num/10;
+        }
+        return count;
+    }
 }
