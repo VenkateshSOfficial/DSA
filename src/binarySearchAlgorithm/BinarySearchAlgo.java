939399bba0302c2a9f845b24bd9790a036a4ece1
Your New Name <Venkatesh.Swaminathan@McKesson.com>
2025-07-23 19:42:46 +0530

    binary code

--- src/binarySearchAlgorithm/BinarySearchAlgo.java
+++ src/binarySearchAlgorithm/BinarySearchAlgo.java
@@ -3,7 +3,7 @@ package binarySearchAlgorithm;
 public class BinarySearchAlgo {
 	public static void main(String[] args) {
 		int[] arr = { -12, -10, -4, 0, 2, 6, 8, 12, 23, 45, 68 };
-		int target = 23;
+		int target = -4;
 		System.out.println("The target element " + target + " is : " + performBinarySearch(arr, target));
 	}
 
