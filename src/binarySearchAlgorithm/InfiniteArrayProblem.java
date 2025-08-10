dfede28b1958bf616797730902d312b59347b756
Your New Name <Venkatesh.Swaminathan@McKesson.com>
2025-08-10 18:28:50 +0530

    infinite array

---
+++ src/binarySearchAlgorithm/InfiniteArrayProblem.java
@@ -0,0 +1,39 @@
+package binarySearchAlgorithm;
+
+public class InfiniteArrayProblem {
+	public static void main(String[] args) {
+		int[] arr={3,5,7,9,10,90,100,130,140,160,170};
+		int target=10;
+		System.out.println(findingRange(arr,target));
+	}
+	static int findingRange(int[] arr, int target){
+		// find the range
+		// first start with box of size 2
+		int start=0;
+		int end=1;
+
+		// condition for the target to in range
+
+		while(target> arr[end]){
+			int temp=end+1;
+			// double the box value
+			// end=previous end + sizeOfBox * 2
+			end=end+(end-start + 1)*2;
+			start=temp;
+		}
+		return binarySearch(arr,target,start,end);
+	}
+	public static int binarySearch(int[] arr, int target,int start,int end){
+		while(start<=end){
+			int mid=start+(end-start)/2;
+			if(target < arr[mid]){
+				end=mid-1;
+			}else if(target > arr[mid]){
+				start=mid+1;
+			}else {
+				return mid;
+			}
+		}
+		return -1;
+	}
+}
